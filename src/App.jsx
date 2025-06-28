529fb11fba3febd6e52f6d2be64addda2c9a9b7a
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-06-28 17:19:05 +0300

    подъем состояния props vs state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = ({ name, price }) => {
-	return <div>{name} - {price} руб</div>
+const Product = ({ name, price, amount }) => {
+	// const [amount, setAmount] = useState(initialAmount);
+
+	return (
+		<>
+			<div>
+				{name} - {price} руб
+			</div>
+			<div>Количество: {amount}</div>
+			<Basket amount={amount} />
+		</>
+	);
 };
 
 Product.PropTypes = {
