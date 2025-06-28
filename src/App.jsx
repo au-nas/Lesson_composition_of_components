c7c885683dc90e601b5ece39dea137ffe0b2d76a
Anastasiia Vasilchenko <nastyavasilshenko@mail.ru>
2025-06-28 16:39:47 +0300

    пример использования PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import PropTypes from 'prop-types';
-import { User, Label} from './components';
-import styles from './App.module.css';
 
-const getUserFromServer = () => ({
-	name: 'Иван',
-	age: 25,
-	email: 'ivan@mail.ru',
-	phone: '+7999-999-99-99',
-});
-
-export const App = () => {
-	const user = getUserFromServer();
-	return (
-		<div className={styles.app}>
-			<Label color={'red'}>Приложение</Label>
-			<div>Разная информация приложения</div>
-			<User {...user} />
-		</div>
-	);
+const Product = ({ name, price }) => {
+	return <div>{name} - {price} руб</div>
 };
 
+Product.PropTypes = {
+	name: PropTypes.string,
+	price: PropTypes.number,
+};
